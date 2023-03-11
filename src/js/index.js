d416527865c2f150b78541f93c0f102aa2d6773d
Filipe Rocha <filiperochaprogramador@gmail.com>
2023-03-11 13:17:47 -0300

    atualização

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+const botoesCarrossel = document.querySelectorAll('.botao')
+const botaoCarrossel = document.querySelector('.botao')
+const imagensFundo = document.querySelectorAll('.imagem')
+
+console.log(botoesCarrossel);
+console.log(botaoCarrossel);
+console.log(imagensFundo);
