f876521d57390e2c8f39014f1a801bb4a2a798af
Filipe Rocha <filiperochaprogramador@gmail.com>
2023-03-11 19:07:45 -0300

    atualização icone

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 const botoesCarrossel = document.querySelectorAll('.botao')
 const botaoCarrossel = document.querySelector('.botao')
 const imagensFundo = document.querySelectorAll('.imagem')
+const carrossel = document.querySelector('.botoes-carrossel')
 
 console.log(botoesCarrossel);
 console.log(botaoCarrossel);
 console.log(imagensFundo);
+console.log(carrossel);
